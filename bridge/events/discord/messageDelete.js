d3c5231ac0ae289c1d7ef804cea7aa502483e3f0
Saboor <42164502+saboooor@users.noreply.github.com>
2022-05-11 12:26:20 -0600

    message delete handler

---
+++ bridge/events/discord/messageDelete.js
@@ -0,0 +1,9 @@
+module.exports = async (discord, guilded, servers, oldmsg, newmsg) => {
+	const srv = servers.find(s => s.discord.serverId == newmsg.guild.id);
+	if (!srv) return;
+	const bridge = srv.channels.find(b => b.discordId == newmsg.channel.id);
+	if (!bridge || !bridge.messages[newmsg.id]) return;
+	const { channelId, id } = bridge.messages[newmsg.id];
+	guilded.messages.delete(channelId, id);
+	delete bridge.messages[newmsg.id]
+};
