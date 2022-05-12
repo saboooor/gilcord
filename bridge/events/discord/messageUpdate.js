945756731134165a2c26c56638e382688399f872
Saboor <saboor547@gmail.com>
2022-05-11 20:12:15 -0600

    parse mentions on messageupdate

--- bridge/events/discord/messageUpdate.js
+++ bridge/events/discord/messageUpdate.js
@@ -1,3 +1,4 @@
+const { MessageMentions: { ChannelsPattern, RolesPattern, UsersPattern } } = require('discord.js');
 module.exports = async (discord, guilded, config, oldmsg, newmsg) => {
 	// Get the server config and check if it exists
 	const srv = config.servers.find(s => s.discord.serverId == newmsg.guild.id);
@@ -6,6 +7,27 @@ module.exports = async (discord, guilded, config, oldmsg, newmsg) => {
 	// Get the channel config and check if it and the cached message exists
 	const bridge = srv.channels.find(b => b.discord.channelId == newmsg.channel.id);
 	if (!bridge || !bridge.messages[newmsg.id]) return;
+
+	// Parse all channel mentions
+	const channelMatches = [...newmsg.content.matchAll(ChannelsPattern)];
+	channelMatches.forEach(match => {
+		const channel = discord.channels.cache.get(match[1]);
+		newmsg.content = newmsg.content.replace(match[0], `#${channel.name}`);
+	});
+
+	// Parse all role mentions
+	const roleMatches = [...newmsg.content.matchAll(RolesPattern)];
+	roleMatches.forEach(match => {
+		const role = newmsg.guild.roles.cache.get(match[1]);
+		newmsg.content = newmsg.content.replace(match[0], `@${role.name}`);
+	});
+
+	// Parse all user mentions
+	const userMatches = [...newmsg.content.matchAll(UsersPattern)];
+	userMatches.forEach(match => {
+		const user = discord.users.cache.get(match[1]);
+		newmsg.content = newmsg.content.replace(match[0], `@${user.tag}`);
+	});
 
 	// Get the nameformat from the configs
 	const nameformat = (bridge.guilded.nameformat ?? srv.guilded.nameformat ?? config.guilded.nameformat).replace(/{name}/g, newmsg.author.tag);
