3bd2ea1c6317510bfd300a1e8819c5415ec058f5
Saboor <saboor547@gmail.com>
2022-05-25 00:22:00 -0600

    we do a little plugging

--- handlers/ready.js
+++ handlers/ready.js
@@ -1,6 +1,6 @@
 const { ActivityType } = require('discord.js');
 module.exports = (client, config) => {
-	client.on('ready', () => {
+	client.on('ready', async () => {
 		client.logger.info('Bot started!');
 		const timer = (Date.now() - client.startTimestamp) / 1000;
 		client.logger.info(`Done (${timer}s)! I am running!`);
@@ -8,9 +8,9 @@ module.exports = (client, config) => {
 		// Set the activity on the discord client
 		if (client.type.name != 'discord') return;
 		if (config.discord.presence) {
-			if (config.discord.presence.status) client.user.setStatus(config.discord.presence.status);
-			if (config.discord.presence.activity) client.user.setPresence({ activities: [{ name: config.discord.presence.activity.name ?? 'Guilded-Discord Bridge', type: ActivityType[config.discord.presence.activity.type ?? 'Playing'] }] });
+			if (config.discord.presence.activity) await client.user.setPresence({ activities: [{ name: config.discord.presence.activity.name ?? 'https://github.com/saboooor/guilded-discord-bridge', type: ActivityType[config.discord.presence.activity.type ?? 'Watching'] }] });
+			await client.user.setStatus(config.discord.presence.status ?? 'dnd');
 		}
-		else { client.user.setPresence({ activities: [{ name: 'Guilded-Discord Bridge' }] }); }
+		else { client.user.setPresence({ activities: [{ name: 'https://github.com/saboooor/guilded-discord-bridge' }] }); }
 	});
 };
