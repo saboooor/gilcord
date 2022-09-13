557caeff6ff3714c52da7f04df7f7f0c21982126
saboooor <saboor547@gmail.com>
2022-09-13 14:31:41 -0600

    fix unknown channel/role/user mentions

--- bridge/functions/parseMentions.js
+++ bridge/functions/parseMentions.js
@@ -6,35 +6,48 @@ const numericDateOnlyTimePattern = /<t:(\d{1,13}):d>/g;
 const dateOnlyTimePattern = /<t:(\d{1,13}):D>/g;
 const shortTimePattern = /<t:(\d{1,13}):t>/g;
 const shortTimeSecondsPattern = /<t:(\d{1,13}):T>/g;
-module.exports = function parseMentions(text, client, guild) {
+module.exports = async function parseMentions(text, client, guild) {
 	let parsed = text;
 
 	// Parse all channel mentions
 	const channelMatches = [...text.matchAll(new RegExp(ChannelsPattern, 'g'))];
-	channelMatches.forEach(match => {
-		const channel = client.channels.cache.get(match[1]);
+	for (const match of channelMatches) {
+		const channel = await client.channels.fetch(match[1]);
+		if (!channel) {
+			client.logger.warn(`Channel Id ${match[1]} wasn't found!`);
+			parsed = parsed.replace(match[0], '**#Unknown Channel**');
+			continue;
+		}
 		parsed = parsed.replace(match[0], `**#${channel.name}**`);
-	});
+	}
 
 	// Parse all role mentions
 	const roleMatches = [...text.matchAll(new RegExp(RolesPattern, 'g'))];
-	roleMatches.forEach(match => {
-		const role = guild.roles.cache.get(match[1]);
+	for (const match of roleMatches) {
+		const role = await guild.roles.fetch(match[1]);
+		if (!role) {
+			client.logger.warn(`Role Id ${match[1]} wasn't found!`);
+			parsed = parsed.replace(match[0], '**@Unknown Role**');
+			continue;
+		}
 		parsed = parsed.replace(match[0], `**@${role.name}**`);
-	});
+	}
 
 	// Parse all user mentions
 	const userMatches = [...text.matchAll(new RegExp(UsersPattern, 'g'))];
-	userMatches.forEach(match => {
-		const user = client.users.cache.get(match[1]);
+	for (const match of userMatches) {
+		const user = await client.users.fetch(match[1]);
+		if (!user) {
+			client.logger.warn(`User Id ${match[1]} wasn't found!`);
+			parsed = parsed.replace(match[0], '**@Unknown User**');
+			continue;
+		}
 		parsed = parsed.replace(match[0], `**@${user.tag}**`);
-	});
+	}
 
 	// Parse all emoji mentions
 	const emojiMatches = [...text.matchAll(EmojisPattern)];
-	emojiMatches.forEach(match => {
-		parsed = parsed.replace(match[0], match[2]);
-	});
+	emojiMatches.forEach(match => parsed = parsed.replace(match[0], match[2]));
 
 	// Parse all timestamps
 	const longTimeMatches = [...text.matchAll(longTimePattern)];
